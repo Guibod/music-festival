78ff0767318e648fea08343f6adb038a46f3842d
Guibod <guibod@sf.net>
2012-06-10 22:14:47 +0200

    Added Track object

---
+++ lib/MusicFestival/Track.php
@@ -0,0 +1,84 @@
+<?php
+
+namespace MusicFestival;
+
+class Track extends \MusicFestival\Entity {
+
+  const ATTR_TITLE = 'title';
+  const ATTR_ARTIST = 'artist';
+  const ATTR_ALBUM = 'album';
+  const ATTR_YEAR = 'year';
+  const ATTR_COVER = 'cover';
+
+  protected $links = array();
+
+  function __construct() {
+    parent::__construct(array(
+      self::ATTR_TITLE,
+      self::ATTR_ARTIST,
+      self::ATTR_ALBUM,
+      self::ATTR_YEAR,
+      self::ATTR_COVER
+    ));
+  }
+
+  /**
+   * @return string
+   */
+  function getTitle() {
+    return $this->getAttribute(self::ATTR_TITLE);
+  }
+
+  /**
+   * @return string
+   */
+  function getArtist() {
+    return $this->getAttribute(self::ATTR_ARTIST);
+  }
+
+  /**
+   * @return string
+   */
+  function getAlbum() {
+    return $this->getAttribute(self::ATTR_ALBUM);
+  }
+
+  /**
+   * @return string
+   */
+  function getYear() {
+    return $this->getAttribute(self::ATTR_YEAR);
+  }
+
+  /**
+   * @return string
+   */
+  function getCover() {
+    return $this->getAttribute(self::ATTR_COVER);
+  }
+
+  /**
+   * @return array<Link>
+   */
+  function getLinks() {
+    return $this->links;
+  }
+
+  /**
+   * @param array $links
+   */
+  function setLinks(array $links) {
+    $this->links = $links;
+  }
+
+  /**
+   * @param array $array
+   * @return \MusicFestival\Track
+   */
+  static function fromArray(array $array) {
+    $track = new Track();
+    $track->setAttributes($array);
+    return $track;
+  }
+
+}
