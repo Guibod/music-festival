ae146ab5c992faaaf13abd775eacc9adaef5610c
Guibod <guibod@sf.net>
2012-06-10 22:05:43 +0200

    Added Person class

--- lib/MusicFestival/Person.php
+++ lib/MusicFestival/Person.php
@@ -2,14 +2,47 @@
 
 namespace MusicFestival;
 
-class Person {
-  protected $name;
+class Person extends Entity {
+  const ATTR_ID   = 'id';
+  const ATTR_NAME = 'name';
+
+  protected $tracks = array();
+
+  function __construct()
+  {
+    parent::__construct(array(
+      self::ATTR_NAME,
+      self::ATTR_ID
+    ));
+  }
 
   /**
    * @return string
    */
   function getName() {
-    return $this->name;
+    return $this->getAttribute(self::ATTR_NAME);
+  }
+
+  /**
+   * @return string
+   */
+  function getId() {
+    return $this->getAttribute(self::ATTR_ID);
+  }
+
+  /**
+   * @return array<Track>
+   */
+  function getTracks() {
+    return $this->tracks;
+  }
+
+  /**
+   * @param array $tracks
+   */
+  function setTracks(array $tracks)
+  {
+    $this->tracks = $tracks;
   }
 
   /**
@@ -18,9 +51,13 @@ class Person {
    */
   static function fromArray(array $array) {
     $person = new Person();
-    $person->name = $array['name'];
+    $person->setAttributes($array);
+
+    if(isset($array['tracks']) && is_array($array['tracks']))
+    {
+      $person->setTracks(Track::fromArray($array['tracks']));
+    }
 
     return $person;
   }
-
 }
